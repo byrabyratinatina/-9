ce2e30a7f9dab03831d8b17869d54d12d7654f13
Andrei <andrei-maiorov@bk.ru>
2022-09-13 21:21:28 +0300

    dot

--- ex001/user/Program.cs
+++ ex001/user/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Readline("Введите ваше имя");
-string username = Console.Readline();
-Console.WriteLine("Привет,");
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
 Console.WriteLine(username);
