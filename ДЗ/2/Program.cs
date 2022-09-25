726d0b0c8074b0d09192fbbcda19e0a62918ac95
Andrei <andrei-maiorov@bk.ru>
2022-09-25 19:37:16 +0300

    les2

---
+++ ДЗ/2/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(10,99999);
+Console.WriteLine($"Число {number}");
+int res = -1;
+while (number > 99)
+{ 
+    res = number%10;
+    number = number/10;
+}
+if (res >=0 )
+{
+    Console.WriteLine($"Третья цифра числа {res} ");
+}
+else Console.WriteLine("Третья цифра отсутствует");
