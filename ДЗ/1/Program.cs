d753f87fa3743e2240fca3e6d51724626da7a3a4
Andrei <andrei-maiorov@bk.ru>
2022-10-05 17:32:47 +0300

    1

--- ДЗ/1/Program.cs
+++ ДЗ/1/Program.cs
@@ -1,32 +1,62 @@
-﻿Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-int count = 0;
-FillArrayRandomNumbers(numbers);
+﻿Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
 PrintArray(numbers);
-
-
+int sum = 0;
 for (int i = 0; i < numbers.Length; i++)
 {
-    if (numbers[i] % 2 == 0)
-    count++;
-}
-
-
-Console.WriteLine($"количество чётных чисел в массиве -> {count} ");
-
-void FillArrayRandomNumbers(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
+    if (numbers[i] > 0)
     {
-        array[i] = new Random().Next(100,1000);
+        sum++;
     }
 }
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
 void PrintArray(int[] array)
 {
-    for(int i = 0; i < array.Length; i++)
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i] + " ");
     }
-    Console.WriteLine();
+    Console.Write("]");
 }
